5ad7ba97b6ad3a2f8766f0db7312c9411e44dc16
SageScroll18144 <ffss@cin.ufpe.br>
2023-09-15 18:39:30 -0300

    implementando o mapeamento das casas

--- src/runner/main.c
+++ src/runner/main.c
@@ -6,8 +6,6 @@
 #include <stdint.h>
 
 int main(void) {
-
-
     const int screenWidth = 800;
     const int screenHeight = 800;
 
@@ -20,12 +18,14 @@ int main(void) {
 
     char board[8][8];
     for(int i=0;i<8;i++) for(int j=0;j<8;j++) board[i][j] = 'x';
-    int turn = 1;
+    
+    Vector2 mouse_pos;
 
     while (!WindowShouldClose()){
         // Update
         //----------------------------------------------------------------------------------
-        
+        mouse_pos = GetMousePosition();
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) printf("(%f, %f)\n", mouse_pos.x, mouse_pos.y);
         //----------------------------------------------------------------------------------
 
         // Draw
