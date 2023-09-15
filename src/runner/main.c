8cb1a993618353e4df368b898b8a5224a3992cbe
SageScroll18144 <ffss@cin.ufpe.br>
2023-09-15 18:46:38 -0300

    implementando a peça de marcação

--- src/runner/main.c
+++ src/runner/main.c
@@ -1,6 +1,6 @@
 #include "raylib.h"
 #include "../scene/background.h"
-#include "../scene/piece.h"
+#include "../scene/mark.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
@@ -12,12 +12,12 @@ int main(void) {
     InitWindow(screenWidth, screenHeight, "bit-chess | Board Status");
 
     setup_background(screenWidth, screenHeight);
-    setup_pieces();
+    setup_mark();
 
     SetTargetFPS(60);
 
     char board[8][8];
-    for(int i=0;i<8;i++) for(int j=0;j<8;j++) board[i][j] = 'x';
+    for(int i=0;i<8;i++) for(int j=0;j<8;j++) board[i][j] = 'M';
     
     Vector2 mouse_pos;
 
@@ -35,14 +35,14 @@ int main(void) {
             ClearBackground(RAYWHITE);
 
             draw_background();
-            draw_pieces(board);
+            draw_mark(board);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
 
     unload_background();
-    unload_pieces();
+    unload_mark();
 
     CloseWindow();                // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
